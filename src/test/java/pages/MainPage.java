68f413b4055454ea308727ba8733391700c8331a
Viktor_Rus <vaurusov@mts.ru>
2022-11-01 11:00:13 +0300

    first commit

---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainPage {
+    private SelenideElement
+            mainLogo = $("a[href='https://www.tutu.ru/']"),
+            iconSearch = $("div[class='search-site']"),
+            inputSearch = $("input[name='s']"),
+            sectionMadeToOrder = $("section[class='made-to-order']"),
+            tabInvestors =  $("a[title='Investors']"),
+            titlePage =  $("h1[class='page-title']"),
+            footer = $("footer[class='footer-wrapper']"),
+            loginFooter =  $x("//a[.='Associate Login']");
+
+
+    @Step("Открытие главной страницы")
+    public MainPage openMainPage() {
+        open(Configuration.baseUrl);
+        return this;
+    }
+
+    @Step("Проверка отображения логотипа на главной странице")
+    public MainPage checkLogoVisible() {
+        mainLogo.should(appear);
+        return this;
+    }
+
+
+}
---
+++ src/test/java/tutu/MainPageTests.java
@@ -0,0 +1,28 @@
+package tutu;
+
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pages.MainPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MainPageTests extends TestBase {
+
+    @BeforeEach
+    public void visitMainPage() {
+        open("/");
+        new MainPage().openMainPage();
+    }
+
+
+    @Test
+    @Owner("vaurusov")
+    @DisplayName("Проверка отображения логотипа на главной странице")
+    void mainLogoVisible() {
+        new MainPage().checkLogoVisible();
+
+    }
+
+}
