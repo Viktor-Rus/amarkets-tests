1d50bf07f5c064453dd600f193b5b45d42355c36
Viktor_Rus <vaurusov@mts.ru>
2022-11-02 13:23:34 +0300

    add proxy

--- src/test/java/tutu/TestBase.java
+++ src/test/java/tutu/TestBase.java
@@ -11,12 +11,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
 
     @BeforeAll
-    static void configure() {
+    static void configure() throws IOException {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -25,18 +31,26 @@ public class TestBase {
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = "https://www.tutu.ru";
+
         Configuration.browserSize = System.getProperty("browser_size");
         Configuration.browser = System.getProperty("browser_name");
         Configuration.browserVersion = System.getProperty("browser_version");
         Configuration.remote = System.getProperty("remote_selenide");
 
+        Configuration.baseUrl = "https://www.tutu.ru";
+
 //        Configuration.proxyEnabled = true;
 //        Configuration.proxyHost = "172.217.169.174";
 //        Configuration.proxyPort = 443;
         Configuration.proxyEnabled = true;
-        System.setProperty("http.proxyHost", "172.217.169.174");
-        System.setProperty("http.proxyPort", "443");
+//        System.setProperty("http.proxyHost", "172.217.169.174");
+//        System.setProperty("http.proxyPort", "443");
+
+        URL weburl = new URL("https://www.tutu.ru");
+        Proxy webProxy
+                = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("172.217.169.174", 443));
+        HttpURLConnection webProxyConnection
+                = (HttpURLConnection) weburl.openConnection(webProxy);
 
 //        WebDriverRunner.getAndCheckWebDriver(); // otherwise WebDriverRunner.getSelenideProxy() returns null
 //
