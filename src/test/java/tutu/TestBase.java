a40041c87c1c66b5b94e75207ab9c3dc33441a75
Viktor_Rus <vaurusov@mts.ru>
2022-11-02 11:18:29 +0300

    add proxy

--- src/test/java/tutu/TestBase.java
+++ src/test/java/tutu/TestBase.java
@@ -25,8 +25,8 @@ public class TestBase {
         Configuration.browserVersion = System.getProperty("browser_version");
         Configuration.remote = System.getProperty("remote_selenide");
 
-        Configuration.proxyHost = "82.114.97.157";
-        Configuration.proxyPort = 1256;
+        Configuration.proxyHost = "90.188.92.116";
+        Configuration.proxyPort = 36335;
     }
 
     @AfterEach
