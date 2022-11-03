de5cf2963b1bc5173c0cb74e051cd7279dc07721
Viktor_Rus <vaurusov@mts.ru>
2022-11-03 06:19:41 +0300

    edit configure

--- src/test/java/tutu/TestBase.java
+++ src/test/java/tutu/TestBase.java
@@ -2,7 +2,6 @@ package tutu;
 
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -22,9 +21,6 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
-        capabilities.setCapability("network.proxy.http", "172.217.169.174");
-        capabilities.setCapability("network.proxy.http_port", 443 );
-
         Configuration.browserCapabilities = capabilities;
 
         Configuration.browserSize = System.getProperty("browser_size");
@@ -32,23 +28,6 @@ public class TestBase {
         Configuration.browserVersion = System.getProperty("browser_version");
         Configuration.remote = System.getProperty("remote_selenide");
 
-//        Configuration.baseUrl = "https://www.tutu.ru";
-
-//        Configuration.proxyEnabled = true;
-//        Configuration.proxyHost = "172.217.169.174";
-//        Configuration.proxyPort = 443;
-//        Configuration.proxyEnabled = true;
-//        System.setProperty("http.proxyHost", "172.217.169.174");
-//        System.setProperty("http.proxyPort", "443");
-
-
-//        WebDriverRunner.getAndCheckWebDriver(); // otherwise WebDriverRunner.getSelenideProxy() returns null
-//
-//        BrowserUpProxy proxy = WebDriverRunner.getSelenideProxy().getProxy();
-//        proxy.setHarCaptureTypes(CaptureType.getAllContentCaptureTypes());
-//        proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
-//        proxy.newHar("pofig");
-//        open("https://google.com");
     }
 
     @AfterEach
