2bf9df4c34be5dcca365504719837ead81e68907
Viktor_Rus <vaurusov@mts.ru>
2022-11-03 15:58:59 +0300

    add second tests

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -56,7 +56,7 @@ public class MainPage {
     public MainPage focusHeaderNavItem(String language) {
         $x("//ul[@class=\"ubermenu-nav\"]//span[text()=\"TRADING\"]").hover();
         $(byText("FAQ")).click();
-        assertEquals($("h4[class='faq__search-title']").getText(), "Enter keywords. For example, \"verification\" or \"how to make a deposit\"");
+        assertEquals($("h4[class='faq__search-title']").getText(), "We've got answers to all of your questions");
         return this;
     }
 
