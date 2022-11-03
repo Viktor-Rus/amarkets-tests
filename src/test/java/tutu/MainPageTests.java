2f2a09d91087b12b0cd1dac38af2a5718e5a3d09
Viktor_Rus <vaurusov@mts.ru>
2022-11-03 11:26:05 +0300

    add second tests

--- src/test/java/tutu/MainPageTests.java
+++ src/test/java/tutu/MainPageTests.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.MainPage;
 
-import static com.codeborne.selenide.Selenide.open;
 
 public class MainPageTests extends TestBase {
 
     @BeforeEach
+    @DisplayName("Открытие главной страницы")
     public void visitMainPage() {
         new MainPage().openMainPage();
     }
